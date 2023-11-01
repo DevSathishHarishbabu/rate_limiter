83bbbfe741790c52eaad5412c7339d9236f73b13
Sathish <sathish@Sathishs-MacBook-Air.local>
2023-11-01 15:33:29 +0530

    Add logging to file and env file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::{net::Ipv4Addr, str::FromStr};
+
 use log::info;
+use dotenv;
 
 mod models;
 mod handlers;
@@ -7,14 +10,18 @@ mod routes;
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+    let host = std::env::var("HOST").unwrap();
+    let addr = Ipv4Addr::from_str(&host).unwrap();
+    let port: u16 = std::env::var("PORT").unwrap().parse().unwrap();
     // Adding loggers 
     log4rs::init_file("logconfig.yml", Default::default()).expect("Log config file not found");
-    info!("Server starting at 3771");
+    info!("Server starting at {}:{}",host,port);
 
     let db = db::init_db();
     let notes_routes = routes::notes_routes(db);
 
     warp::serve(notes_routes)
-        .run(([127,0,0,1], 3771))
+        .run((addr, port))
         .await;
 }
